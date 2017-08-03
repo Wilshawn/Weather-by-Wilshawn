e818e1a0afa89fc6391a54a6d36ae6b016be3d68
Wilshawn <wilshawn416@verizon.net>
2017-08-02 22:40:25 -0400

    Zip-Code-Function - Add Zip Code Function

--- JS/getWeather.js
+++ JS/getWeather.js
@@ -1,8 +1,25 @@
-function getWeather() {
+function getWeatherByCity() {
   /* Resets City Temp */
   $('.weatherResponse').html('');
   var cityName = $('#cityName').val();
   var apiCall = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=4a90aee1c5a6a311869b8fa68d87623d';
+  $.getJSON(apiCall, weatherCallback);
+  
+  /* Sends in Weather */
+  function weatherCallback(weatherData) {
+    var weatherName = weatherData.name;
+    var weatherCountry = weatherData.sys.country;
+    var weatherDescription = weatherData.weather[0].description;
+    var weatherTemperature = Math.round((weatherData.main.temp - 273.15) * 9/5 + 32);
+    $('.weatherResponse').append("The weather in " + weatherName + "," + weatherCountry + " is " + weatherTemperature + " degrees and " + weatherDescription);
+  }
+}
+
+function getWeatherByZip() {
+  /* Resets City Temp */
+  $('.weatherResponse').html('');
+  var zipCode = $('#zipCode').val();
+  var apiCall = 'http://api.openweathermap.org/data/2.5/weather?zip=' + zipCode + '&appid=4a90aee1c5a6a311869b8fa68d87623d';
   $.getJSON(apiCall, weatherCallback);
   
   /* Sends in Weather */
